5bdb414ca5cba5ec5f57e34c0f1ebfee5451f1bd
Eleanor Kahn <eleanor.c.kahn@gmail.com>
2023-02-16 12:55:01 -0800

    Adding router functionality to HomePage, ContactPage, and CampsitesDirectory using React Router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import { Routes, Route } from "react-router-dom";
 import './App.css';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Header from "./components/Header";
 import Footer from "./components/Footer"
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
+
 
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesDirectoryPage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="directory" element={<CampsitesDirectoryPage />} />
+      </Routes>
       <Footer />
     </div>
   );
---
+++ src/pages/ContactPage.js
@@ -0,0 +1,9 @@
+
+
+const ContactPage = () => {
+    return (
+        <div>ContactPage</div>
+    );
+};
+
+export default ContactPage;
