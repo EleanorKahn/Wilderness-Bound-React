8ceee0d1d856385519b8fbe29d81703a6b78cd6c
Eleanor Kahn <eleanor.c.kahn@gmail.com>
2023-02-20 18:17:06 -0800

    AnimatedDisplayCard with React Spring

---
+++ src/features/display/AnimatedDisplayCard.js
@@ -0,0 +1,34 @@
+// DisplayCard is an example of a presentational component - doesn't really use any logic, just presents
+import { useState, useEffect } from "react";
+import{ Card, CardImg, CardText, CardTitle, CardBody } from "reactstrap";
+import { useSpring, animated } from "react-spring";
+
+const AnimatedDisplayCard= ({ item }) => {
+    const { image, name, description } = item;
+    const [toggle, setToggle] = useState(false);
+
+    const animatedStyle = useSpring({
+        opacity: toggle ? 1 : 0,
+        transform: toggle ? "scale(1,1)" : "scale(1,0)",
+        config: { duration: 500 }
+    });
+
+    useEffect(() => {
+        setToggle(true);
+        //empty array signifies this effect should only run on first mount
+    }, []);
+
+    return (
+        <animated.div style={animatedStyle}>
+            <Card>
+                <CardImg src={image} alt={name} />
+                <CardBody>
+                    <CardTitle>{name}</CardTitle>
+                    <CardText>{description}</CardText>
+                </CardBody>
+            </Card>
+        </animated.div>
+    );
+};
+
+export default AnimatedDisplayCard;
--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,20 +1,25 @@
 import { Col, Row } from "reactstrap";
-import DisplayCard from "./DisplayCard";
+//import DisplayCard from "./DisplayCard";
 import { selectFeaturedCampsite } from "../campsites/campsitesSlice";
 import { selectFeaturedPromotion} from "../promotions/promotionsSlice";
 import { selectFeaturedPartner } from "../partners/partnerSlice";
+import AnimatedDisplayCard from "./AnimatedDisplayCard";
 
 const DisplayList = () => {
-    const featuredItems = [selectFeaturedCampsite(), selectFeaturedPromotion(), selectFeaturedPartner()];
+    const featuredItems = [
+        selectFeaturedCampsite(), selectFeaturedPromotion(), selectFeaturedPartner()
+    ];
 
     return (
         <Row>
             {featuredItems.map((item, idx) => {
                 return (
-                    <Col md className="m-1" key={idx}>
-                        <DisplayCard item={item}></DisplayCard>
-                    </Col>
-                )
+                    item && (
+                        <Col md className="m-1" key={idx}>
+                            <AnimatedDisplayCard item={item}></AnimatedDisplayCard>
+                        </Col>
+                    )
+                );
             })}
         </Row>
     );
