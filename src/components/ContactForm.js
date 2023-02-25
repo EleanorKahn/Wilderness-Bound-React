5ad29310e32fbb0e946344d2b1a673907bdedf51
Eleanor Kahn <eleanor.c.kahn@gmail.com>
2023-02-25 11:00:49 -0800

    validateContactForm functionality complete, typo in options tag fixed

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,6 +1,7 @@
 import { Button, Label, Col, FormGroup } from "reactstrap";
-import { Formik, Field, Form } from "Formik";
-import React from 'react'
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import React from "react"
+import { validateContactForm } from "../utils/validateContactForm";
 
 const ContactForm = () => {
     const handleSubmit = (values, { resetForm }) => {
@@ -20,38 +21,51 @@ const ContactForm = () => {
                 feedback: "",
             }}
             onSubmit={handleSubmit}
+            validate={validateContactForm}
         >
             <Form>
                 <FormGroup row>
-                    <Label htmlFor='firstName' md='2'>
+                    <Label htmlFor="firstName" md="2">
                         First Name
                     </Label>
-                    <Col md='10'>
+                    <Col md="10">
                         <Field className="form-control" name="firstName" placeholder="First Name" />
+                        <ErrorMessage name="firstName">
+                            {(msg) => <p className="text-danger">{msg}</p>}
+                        </ErrorMessage>
                     </Col>
                 </FormGroup>
                 <FormGroup row>
-                    <Label htmlFor='lastName' md='2'>
+                    <Label htmlFor="lastName" md="2">
                         Last Name
                     </Label>
-                    <Col md='10'>
+                    <Col md="10">
                         <Field className="form-control" name="lastName" placeholder="Last Name" />
+                        <ErrorMessage name="lastName">
+                            {(msg) => <p className="text-danger">{msg}</p>}
+                        </ErrorMessage>
                     </Col>
                 </FormGroup>
                 <FormGroup row>
-                    <Label htmlFor='phoneNum' md='2'>
+                    <Label htmlFor="phoneNum" md="2">
                         Phone
                     </Label>
-                    <Col md='10'>
+                    <Col md="10">
                         <Field className="form-control" name="phoneNum" placeholder="Phone" />
+                        <ErrorMessage name="phoneNum">
+                            {(msg) => <p className="text-danger">{msg}</p>}
+                        </ErrorMessage>
                     </Col>
                 </FormGroup>
                 <FormGroup row>
-                    <Label htmlFor='email' md='2'>
+                    <Label htmlFor="email" md="2">
                         Email
                     </Label>
-                    <Col md='10'>
+                    <Col md="10">
                         <Field className="form-control" name="email" placeholder="Email" />
+                        <ErrorMessage name="email">
+                            {(msg) => <p className="text-danger">{msg}</p>}
+                        </ErrorMessage>
                     </Col>
                 </FormGroup>
                 <FormGroup row>
@@ -63,18 +77,18 @@ const ContactForm = () => {
                         />{" "}
                         May we contact you?
                     </Label>
-                    <Col md='4'>
+                    <Col md="4">
                         <Field className="form-control" name="contactType" as="select">
                             <option>By Phone</option>
-                            <obtion>By Email</obtion>
+                            <option>By Email</option>
                         </Field>
                     </Col>
                 </FormGroup>
                 <FormGroup row>
-                    <Label htmlFor='feedback' md='2'>
+                    <Label htmlFor="feedback" md="2">
                         Your Feedback
                     </Label>
-                    <Col md='10'>
+                    <Col md="10">
                         <Field className="form-control" name="feedback" as="textarea" rows="12" />
                     </Col>
                 </FormGroup>
--- src/utils/validateContactForm.js
+++ src/utils/validateContactForm.js
@@ -6,7 +6,7 @@ export const validateContactForm = (values) => {
 
     if (!firstName) {
         errors.firstName = "Required";
-    } else if (firstName.length > 1) {
+    } else if (firstName.length < 2) {
         errors.firstName = "Must be at least 2 characters";
     } else if (firstName.length > 15) {
         errors.firstName = "Must be 15 characters or fewer";
@@ -14,7 +14,7 @@ export const validateContactForm = (values) => {
 
     if (!lastName) {
         errors.lastName = "Required";
-    } else if (lastName.length > 1) {
+    } else if (lastName.length < 2) {
         errors.lastName = "Must be at least 2 characters";
     } else if (lastName.length > 15) {
         errors.lastName = "Must be 15 characters or fewer";
