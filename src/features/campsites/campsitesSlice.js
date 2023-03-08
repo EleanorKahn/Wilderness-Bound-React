81924235aec7c30a8a3718e30448967ef3eadf96
Eleanor Kahn <eleanor.c.kahn@gmail.com>
2023-03-07 16:01:00 -0800

    mapImageURL util file

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,4 +1,4 @@
-import { CAMPSITES } from "../../app/shared/CAMPSITES";
+import { CAMPSITES } from "../../app/shared/oldData/CAMPSITES";
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
---
+++ src/utils/mapImageURL.js
@@ -0,0 +1,10 @@
+import { baseUrl } from "../app/shared/baseUrl";
+
+export const mapImageUrl = (arr) => {
+    return arr.map((item) => {
+        return {
+            ...item,
+            image: baseUrl + item.image,
+        }
+    })
+}
